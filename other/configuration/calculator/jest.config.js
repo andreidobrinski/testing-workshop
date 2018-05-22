c14dd75dca69afe6f2b82f4430fba28bafc9deb1
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-05-22 16:49:20 -0400

    add code coverage reports

--- other/configuration/calculator/jest.config.js
+++ other/configuration/calculator/jest.config.js
@@ -4,8 +4,19 @@ module.exports = {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock'),
   },
-  setupTestFrameworkScriptFile: require.resolve('./test/setup-test-framework')
+  setupTestFrameworkScriptFile: require.resolve('./test/setup-test-framework'),
+  collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 18,
+      branches: 10,
+      functions: 19,
+      lines: 18,
+    },
+  },
 }
+
+// collectCoverageFrom helps to not count test files in the coverage
 
 // npm install --save-dev identity-obj-proxy
 // use identity-obj-proxy for css modules
