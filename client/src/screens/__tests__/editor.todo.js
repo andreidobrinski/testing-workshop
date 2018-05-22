51cd6c828740d93730231f6e98efbe3a53a026c0
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-05-22 17:32:24 -0400

    complete - unit testing a react component

--- client/src/screens/__tests__/editor.todo.js
+++ client/src/screens/__tests__/editor.todo.js
@@ -1,4 +1,51 @@
-test('calls onSubmit with the username and password when submitted', () => {
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as utilsMock from '../../utils/api'
+import Editor from '../editor.todo'
+
+jest.mock('../../utils/api', () => {
+  return {
+    posts: {
+      create: jest.fn(() => Promise.resolve()),
+    },
+  }
+})
+
+const flushPromises = () => {
+  return new Promise(resolve => {
+    setTimeout(resolve, 0)
+  })
+}
+
+test('calls onSubmit with the username and password when submitted', async () => {
+  const container = document.createElement('div')
+  const fakeUser = {id: 'foobar'}
+  const fakeHistory = {
+    push: jest.fn(),
+  }
+  ReactDOM.render(<Editor user={fakeUser} history={fakeHistory} />, container)
+  const form = container.querySelector('form')
+  const {title, content, tags} = form.elements
+  title.value = 'this is the title'
+  content.value = 'this is the content'
+  tags.value = 'these, are, the, tags'
+
+  const submit = new window.Event('submit')
+  form.dispatchEvent(submit)
+
+  await flushPromises()
+
+  expect(fakeHistory.push).toHaveBeenCalledTimes(1)
+  expect(fakeHistory.push).toHaveBeenCalledWith('/')
+  expect(utilsMock.posts.create).toHaveBeenCalledTimes(1)
+  expect(utilsMock.posts.create).toHaveBeenCalledWith({
+    authorId: fakeUser.id,
+    title: title.value,
+    content: content.value,
+    tags: ['these', 'are', 'the', 'tags'],
+    date: expect.any(String),
+  })
+
   // Arrange
   // create a fake user, post, history, and api
   //
