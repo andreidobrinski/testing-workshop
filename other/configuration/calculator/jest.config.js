033d64b2421c151870db74c9d499bddc581707ef
Andrei Dobrinski <andreiidobrinski@gmail.com>
2018-05-22 16:28:42 -0400

    complete css imports

---
+++ other/configuration/calculator/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy',
+    '\\.css$': require.resolve('./test/style-mock'),
+  },
+}
+
+// npm install --save-dev identity-obj-proxy
+// use identity-obj-proxy for css modules
+
+// if a module is running in both the server and the browser,
+// test it in the browser.
+// the more difficult environment is the browser
